7c521d53dee277bf2fddff2acbd95d138923b03d
ChampionSwimmer <android@championswimmer.tk>
2013-02-07 03:05:45 +0530

    update fingerprint
    Change-Id: Ic1ac10d74ddffdb81c20d58280c8cedbb36f2910
    Signed-off-by: ChampionSwimmer <android@championswimmer.tk>
    
    Conflicts:
    
    	cm.mk

--- cm.mk
+++ cm.mk
@@ -13,7 +13,8 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT25i_1268-6904 BUILD_FINGERPRINT=Sony/LT25i_1268-6904/LT25i:4.0.4/9.0.D.0.484/Gz7_tw:user/release-keys PRIVATE_BUILD_DESC="LT25i-user 4.0.4 9.0.D.0.484 Gz7_tw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT25i BUILD_FINGERPRINT=Sony/LT25i_1268-6904/LT25i:4.1.2/9.1.A.0.490/Ff5_tw:user/release-keys PRIVATE_BUILD_DESC="LT25i-user 4.1.2 9.1.A.0.490 Ff5_tw test-keys"
+
 
 PRODUCT_NAME := cm_tsubasa
 PRODUCT_DEVICE := tsubasa

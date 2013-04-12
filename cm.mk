9b31b2084dcd5aa45cb751415c5a94561cee25fd
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-04-13 00:10:40 +0200

    tsubasa: fix private descr
    Change-Id: Icb321fe3ae4946f255d21c0dea2f6fb967f5953e

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT25i BUILD_FINGERPRINT=Sony/LT25i_1268-6904/LT25i:4.1.2/9.1.A.1.140/c_t_tw:user/release-keys PRIVATE_BUILD_DESC="4.1.2 9.1.A.1.140 c_t_tw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT25i BUILD_FINGERPRINT=Sony/LT25i_1268-6904/LT25i:4.1.2/9.1.A.1.140/c_t_tw:user/release-keys PRIVATE_BUILD_DESC="LT25i-user 4.1.2 9.1.A.1.140 c_t_tw test-keys"
 
 
 PRODUCT_NAME := cm_tsubasa

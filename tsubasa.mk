d23872c5662f92f22d4134a369f2b6036e906d6b
chrmhoffmann <chrmhoffmann@gmail.com>
2015-05-10 14:23:03 +0200

    tsubasa: Remove density from PRODUCT_AAPT_CONFIG
    Change-Id: Ic050f1954eea0206e4c1ec1577ac3525160c2cc7

--- tsubasa.mk
+++ tsubasa.mk
@@ -23,11 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/tsubasa/overlay
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-# This device is xhdpi.  However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # HW Settings

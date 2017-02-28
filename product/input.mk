da6ccb2c16e3b1b28ffaeef150513483f52c8729
Jaekyun Seok <jaekyun@google.com>
2017-02-28 18:08:32 +0900

    product: Move input device config files into vendor partition
    Change-Id: I848718a8118d88da1f570575c3243bc072a6c6e1

--- product/input.mk
+++ product/input.mk
@@ -1,3 +1,3 @@
 # Touchscreen configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc
+    $(LOCAL_PATH)/configs/sensor00_f11_sensor0.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/sensor00_f11_sensor0.idc

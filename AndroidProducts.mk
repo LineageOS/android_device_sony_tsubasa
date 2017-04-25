Manuellama/aosp_development_sony8960_n

build: Add Android AOSP build support
 * Add aosp_mint vendorsetup
 * Add aosp_mint.mk with BOARD_AOSP_BASED enabled

Change-Id: I49ec15dddc775ce5d294b33e4846b7382176e402
Signed-off-by: AdrianDC <radian.dc@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_mint.mk

vendorsetup.sh
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo aosp_mint-eng
+add_lunch_combo aosp_mint-userdebug

LineageOS/android_device_sony_tsubasa

 * Add aosp_tsubasa vendorsetup
 * Add aosp_tsubasa.mk with BOARD_AOSP_BASED enabled

AndroidProducts.mk
@@ -1,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_tsubasa.mk

vendorsetup.sh
@@ -1,0 +1,18 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo aosp_tsubasa-eng
+add_lunch_combo aosp_tsubasa-userdebug